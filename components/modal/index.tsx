ac1fe1bc5c0bb226168097681936822ea9988c3b
Alireza Abbasi <alireza_physicist@yahoo.com>
2023-11-14 10:02:12 +0330

    make responsive

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -21,16 +21,13 @@ const Modal: React.FC<ModalProps> = (props) => {
   } = props;
   return (
     <div
-      className={`transition-all ease-in-out duration-700 ${
+      className={`w-full max-h-[90%] overflow-auto transition-all ease-in-out duration-700 ${
         toggleModal
           ? "fixed opacity-100"
           : "fixed translate-y-[-400px] opacity-0"
       }`}
-      // className={`duration-600 transition-all ease-in-out bg-transparent bg-opacity-50 backdrop-blur-sm z-50 h-screen w-screen flex justify-center items-center ${
-      //   toggleModal ? "fixed" : "absolute top-[-1000px]"
-      // }`}
     >
-      <div className="relative w-max min-w-[400px] m-auto h-max bg-purple-100 p-3 rounded-lg flex flex-col justify-center items-center gap-4">
+      <div className="relative w-max max-w-[90%] m-auto h-max bg-purple-100 p-3 rounded-lg flex flex-col justify-center items-center gap-4">
         {closeBtn && (
           <XMarkIcon
             className="absolute w-4 right-4 top-4 cursor-pointer"
